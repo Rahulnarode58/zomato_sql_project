037205f7685a3719ba01fb893bd73524b4c9befb
Rahul Narode <98022032+Rahulnarode58@users.noreply.github.com>
2025-06-16 14:04:48 +0530

    Delete zomato_schema_and_queries.sql

--- zomato_schema_and_queries.sql
+++
@@ -1,8 +0,0 @@
-Restaurants
-
-CREATE TABLE Restaurants (
-    restaurant_id INT AUTO_INCREMENT PRIMARY KEY,
-    restaurant_name VARCHAR(100),
-    city VARCHAR(50),
-    opening_hours VARCHAR(50)
-);
