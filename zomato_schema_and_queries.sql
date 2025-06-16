a9f6e1d0959a716948749777485b6b2b45b53bea
Rahul Narode <98022032+Rahulnarode58@users.noreply.github.com>
2025-06-16 13:58:04 +0530

    Create zomato_schema_and_queries.sql

---
+++ zomato_schema_and_queries.sql
@@ -0,0 +1,8 @@
+Restaurants
+
+CREATE TABLE Restaurants (
+    restaurant_id INT AUTO_INCREMENT PRIMARY KEY,
+    restaurant_name VARCHAR(100),
+    city VARCHAR(50),
+    opening_hours VARCHAR(50)
+);
